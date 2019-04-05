70d48c79ef3dc8800f63736a41ec0b6214d98b6a
AmirF27 <amir.f.aldeen@gmail.com>
2019-04-05 13:40:07 +0300

    Suppress errors for branch check script

--- scripts/check-branch.sh
+++ scripts/check-branch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-branch=$(git symbolic-ref --short HEAD)
+branch=$(git symbolic-ref --short HEAD 2> /dev/null)
 
-if [ $? != 0 ]
+if [ $? != 0 ]; then
   echo "You must be on a branch in order to commit."
   exit 1
-elif [ "$branch" == "master" ]
+elif [ "$branch" == "master" ]; then
   echo "Committing on the master branch is not allowed."
   exit 1
 fi
