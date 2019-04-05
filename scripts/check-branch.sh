f1f85115ad1ed2fcf77f9621ccdb37cda18e821c
AmirF27 <amir.f.aldeen@gmail.com>
2019-04-05 13:34:17 +0300

    Extend script to allow committing only on branches

--- scripts/check-branch.sh
+++ scripts/check-branch.sh
@@ -2,8 +2,10 @@
 
 branch=$(git symbolic-ref --short HEAD)
 
-if [ "$branch" == "master" ]
-then
+if [ $? != 0 ]
+  echo "You must be on a branch in order to commit."
+  exit 1
+elif [ "$branch" == "master" ]
   echo "Committing on the master branch is not allowed."
   exit 1
 fi
